293b745ae4e89516d7157768dad7882626d2c96a
xiongrui <m15801983447@163.com>
2020-07-06 19:38:36 +0800

    create object

---
+++ src/main/java/com/allen/tuning/TuningDemoApplication.java
@@ -0,0 +1,13 @@
+package com.allen.tuning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuningDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuningDemoApplication.class, args);
+    }
+
+}
