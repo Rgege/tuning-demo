32acb5c239580549e58b5ef4f55193c39c93c7f7
xiongrui <m15801983447@163.com>
2020-07-09 16:44:23 +0800

    commit

--- src/main/java/com/allen/tuning/entity/rsp/data/DeadLockRsp.java
+++ src/main/java/com/allen/tuning/entity/rsp/data/DeadLockRsp.java
@@ -8,4 +8,14 @@ import com.allen.tuning.entity.rsp.AbstractData;
  */
 public class DeadLockRsp extends AbstractData {
     private static final long serialVersionUID = 1843308450865411045L;
+
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
